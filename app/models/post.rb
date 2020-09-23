c148b9d481d5edc903d69dc50a4e89069c88559b
mimizudansyaku <sknm2525@docomo.ne.jp>
2020-09-23 19:18:30 +0900

    add menu_post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :image, presence: true
+  validates :comment, length: { maximum: 300 }
+  validates :user_id, presence: true
+
+  def user
+    User.find(self.user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,8 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  def posts
+    Post.where(user_id: self.id)
+  end
 end
